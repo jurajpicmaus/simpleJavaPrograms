73ed281419fbdd28eade7b3419e51586a54da353
jurajpicmaus <juropico@gmail.com>
2021-05-16 23:14:07 +0200

    Added do-while cycle in Calculator class

--- src/sk/juro/Calculator.java
+++ src/sk/juro/Calculator.java
@@ -11,28 +11,34 @@ public class Calculator {
         double numberOne = input.nextDouble();
         System.out.println("Zadaj druhe cislo");
         double numberTwo = input.nextDouble();
-        System.out.println("Aku operaciu chces vykonat?\n a)scitanie\n b)odcitanie\n c)nasobenie\n d)delenie");
-        String znak = input.next();
+        int checkPoint;
+        do{
+            checkPoint =0;
+            System.out.println("Aku operaciu chces vykonat?\n a)scitanie\n b)odcitanie\n c)nasobenie\n d)delenie");
+            String znak = input.next();
 
-        switch (znak) {
-            case "a":
-                double vysledok = Operacie.add(numberOne, numberTwo);
-                System.out.println("Vysledok scitania je " + vysledok);
-                break;
-            case "b":
-                vysledok = Operacie.subtract(numberOne, numberTwo);
-                System.out.println("Vysledok odcitania je " + vysledok);
-                break;
-            case "c":
-                vysledok = Operacie.multiply(numberOne, numberTwo);
-                System.out.println("Vysledok nasobenia je " + vysledok);
-                break;
-            case "d":
-                vysledok = Operacie.divide(numberOne, numberTwo);
-                System.out.println("Vysledok delenia je " + vysledok);
-                break;
-            default:
-                System.out.println("Nevybral si si ziadnu operaciu");
-        }
+            switch (znak) {
+                case "a":
+                    double vysledok = Operacie.add(numberOne, numberTwo);
+                    System.out.println("Vysledok scitania je " + vysledok);
+                    break;
+                case "b":
+                    vysledok = Operacie.subtract(numberOne, numberTwo);
+                    System.out.println("Vysledok odcitania je " + vysledok);
+                    break;
+                case "c":
+                    vysledok = Operacie.multiply(numberOne, numberTwo);
+                    System.out.println("Vysledok nasobenia je " + vysledok);
+                    break;
+                case "d":
+                    vysledok = Operacie.divide(numberOne, numberTwo);
+                    System.out.println("Vysledok delenia je " + vysledok);
+                    break;
+                default:
+                    System.out.println("Nevybral si si ziadnu operaciu\nVyskusaj znova");
+                    System.out.println();
+                    checkPoint=1;
+            }
+        }while (checkPoint==1);
     }
 }
